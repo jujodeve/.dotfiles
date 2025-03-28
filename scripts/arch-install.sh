e98aac5b2c21ad23c6cee58d71bd8f886fe1e1d8
jotix <jujodeve@gmail.com>
2025-03-28 00:08:29 -0300

    passwords working

--- scripts/arch-install.sh
+++ scripts/arch-install.sh
@@ -6,12 +6,12 @@ set -e
 set_password() {
     PASS1=""
     PASS2=""
-    while [[ "$PASS1" != "$PASS2" ]] || [[ ${#PASS1} < $2 ]]; do
+    while [[ "$PASS1" != "$PASS2" ]] ||  (( ${#PASS1} < $2 )); do
         read -sp "$1 (minimum $2 chars): " PASS1; echo >&2
         read -sp "confirm password: " PASS2; echo >&2
         if  [[ "$PASS1" != "$PASS2" ]]; then
             echo "the passwords don't match." >&2
-        elif [[ $(count_chars $PASS1) < $2 ]]; then
+        elif (( ${#PASS1} < $2 )); then
             echo "the password is too short." >&2
         fi
     done
